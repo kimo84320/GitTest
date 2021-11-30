efbb12e4a0b4efa3789e33d189d06aa5f387a655
LAPTOP-S44OU0EK\kimo8 <kimo84320@gmail.com>
2021-11-30 15:20:53 +0800

    3rd Version

--- GitSample/src/GitApp/AppMain.java
+++ GitSample/src/GitApp/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2nd Version");
+		System.out.println("3rd Version");
 	}
 
 }
