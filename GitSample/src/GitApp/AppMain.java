01b4137730c5c6880eafbdb25b373f9cfc90160e
LAPTOP-S44OU0EK\kimo8 <kimo84320@gmail.com>
2021-11-30 15:34:19 +0800

    Add MathClass

--- GitSample/src/GitApp/AppMain.java
+++ GitSample/src/GitApp/AppMain.java
@@ -5,6 +5,15 @@ public class AppMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("3rd Version");
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
+		
 	}
 
+}
+class Math {
+	int add(int a,int b) {
+		return a+b;
+	}
 }
